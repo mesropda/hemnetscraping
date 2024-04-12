f5682eabdc98ad264e13974d5827ba424ea00878
Mesrop Daghbashyan <mesrop01@yahoo.com>
2024-04-12 11:26:44 +0200

    uådates for vercel

--- hemnetscraping/wsgi.py
+++ hemnetscraping/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hemnetscraping.settings')
 
 application = get_wsgi_application()
+
+# Doing this for vercell
+app = application
